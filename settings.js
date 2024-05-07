01330cb4c6480c5187bbcfbff5453836d538f234
tanpa-batas <166152592+tanpa-batas@users.noreply.github.com>
2024-05-07 23:09:33 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
+global.creator = '👑 API OFFICIAL 👑'// yourname
+global.MONGO_DB_URI = "mongodb+srv://zeltoria1909:96xAwEIGoHhOO2lR@cluster0.v5c9etv.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
+global.your_email = "kgfaps@gmail.com" //email
+global.email_password = "idispbrhgbyhlcib" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
